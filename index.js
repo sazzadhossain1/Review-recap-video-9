fdba81570905c4ebaa2a95c275a68c3d5b27b4e1
sazzadhossain1 <sazzadtomal1@gmail.com>
2022-05-27 17:15:11 +0600

    second commit

--- index.js
+++ index.js
@@ -3,15 +3,25 @@ const productPrice = 100;
 let freeShipping = false;
 let handPicked = false;
 
-if (productPrice >= 100) {
-  freeShipping = true;
-} else if (productPrice >= 60) {
-  freeShipping = false;
-  handPicked = true;
-} else {
-  freeShipping = false;
-  handPicked = false;
-}
+// if (productPrice >= 100) {
+//   freeShipping = true;
+// } else if (productPrice >= 60) {
+//   freeShipping = false;
+//   handPicked = true;
+// } else {
+//   freeShipping = false;
+//   handPicked = false;
+// }
 
+freeShipping =
+  productPrice >= 100
+    ? "product bashay pochey dibo"
+    : "tar product se nihe niye jabe";
+
+// handPicked = productPrice > 60 && productPrice < 99 ? true : false;
+handPicked =
+  productPrice > 60 || productPrice < 99
+    ? "free shipping"
+    : "tar product se nihe niye jabe";
 console.log(freeShipping);
 console.log(handPicked);
